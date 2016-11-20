1a4efed758ccf563704bf8dbf739bab9f6697a75
陈诚 <chencheng0816@gmail.com>
2016-11-20 23:13:40 +0800

    哈哈哈

--- testJFinal/src/cn/testJFinal/controller/IndexController.java
+++ testJFinal/src/cn/testJFinal/controller/IndexController.java
@@ -12,6 +12,6 @@ public class IndexController extends Controller {
 		String sayHello = "Hello " + userName;
 		this.setAttr("sayHello", sayHello);
 		this.render("/hello.jsp");
-		System.out.println("我的第一个JFinalDemo！");
+		System.out.println("我的JFinalDemo！");
 	}
 }
